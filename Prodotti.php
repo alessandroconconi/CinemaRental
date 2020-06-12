40c0b5db26dcbf834d000562592ca325772b16aa
alessandroconconi <alessandro.conconi2@gmail.com>
2020-06-12 16:04:10 +0200

    fix Visualizza.php

--- Prodotti.php
+++ Prodotti.php
@@ -21,7 +21,7 @@ $query=mysqli_query($con,$sql);
 foreach ($query as $riga){
   echo "<tr><td>";
   echo "<img src=img/".$riga["Immagine"]." width=400 length=400/><br>"; //iMMAGINE
-  echo "<a href=visualizza.php?id=".$riga["id"].">".$riga["Marca"]." ".$riga["Modello"]."</a><br>"; //MARCA E MODELLO CHE PORTANO ALLA PAGINA DEL PRODOTTO (SI PUO SOSTITUIRE CON UN PULSANTE)
+  echo "<a href=Visualizza.php?id=".$riga["id"].">".$riga["Marca"]." ".$riga["Modello"]."</a><br>"; //MARCA E MODELLO CHE PORTANO ALLA PAGINA DEL PRODOTTO (SI PUO SOSTITUIRE CON UN PULSANTE)
   echo "</td></tr>";
 }
 
